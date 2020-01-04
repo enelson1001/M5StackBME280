ed6efed19da54f3f1cafc69cba3665f090d14cb0
Ed Nelson <ednelson5080@gmail.com>
2020-01-04 09:54:02 -0700

    cleanup

--- main/files.cmake
+++ main/files.cmake
@@ -7,19 +7,11 @@ set(SOURCES
         gui/LvglTask.cpp
         gui/LvglTask.h
 
-<<<<<<< HEAD
         gui/ViewController.cpp
         gui/ViewController.h
 
         gui/DisplayDriver.cpp
         gui/DisplayDriver.h
-=======
-        gui/Bme280View.cpp
-        gui/Bme280View.h
-
-        gui/LvDisplayDriver.cpp
-        gui/LvDisplayDriver.h
->>>>>>> cdaf9695e0182962cfecd6d15f73a70dcc143e78
 
         gui/GuiButton.cpp
         gui/GuiButton.h
@@ -29,7 +21,6 @@ set(SOURCES
         gui/GuiButtonNext.h
         gui/HwPushButton.h
 
-<<<<<<< HEAD
         gui/TitlePane.cpp
         gui/TitlePane.h
 
@@ -45,39 +36,14 @@ set(SOURCES
         gui/CPHourlyLog.h
         gui/CPTimeAndDate.cpp
         gui/CPTimeAndDate.h
-=======
-        gui/TitleBar.cpp
-        gui/TitleBar.h
-
-        gui/MenuBar.cpp
-        gui/MenuBar.h
-
-        gui/IContainer.h
-        gui/ContentPane001.cpp
-        gui/ContentPane001.h
-        gui/ContentPane002.cpp
-        gui/ContentPane002.h
-        gui/ContentPane003.cpp
-        gui/ContentPane003.h
-        gui/ContentPane004.cpp
-        gui/ContentPane004.h
->>>>>>> cdaf9695e0182962cfecd6d15f73a70dcc143e78
 
         sntp/WifiSsidPasswd.h
         sntp/SntpTask.cpp
         sntp/SntpTask.h
-<<<<<<< HEAD
         sntp/TimeValue.h
         
         model/PollSensorTask.cpp
         model/PollSensorTask.h
         model/EnvirValue.h
-=======
-        sntp/DateTimeValue.h
-        
-        model/ScanSensorTask.cpp
-        model/ScanSensorTask.h
-        model/Bme280Measurements.h
->>>>>>> cdaf9695e0182962cfecd6d15f73a70dcc143e78
         )
 
